4537795c03f8f870b36c6cc16e1e03b83078a7e9
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:37:17 +0300

    Add files via upload

---
+++ work-31.py
@@ -0,0 +1,75 @@
+# if26
+#print("\nif26 - X kiriting")
+#x=float(input())
+
+#if x<=0:
+#    f=-x
+#elif x<2:
+#    f=x*x
+#else:
+ #   f=4
+
+#print("f(x)=",f)
+
+## if27
+#print("\nif27 - X kiriting")
+#x=float(input())
+
+#if x<0:
+#    f=0
+#elif int(x)%2==0:
+#    f=1
+#else:
+#    f=-1
+
+#print("f(x)=",f)
+
+## if28
+#print("\nif28 - Yil kiriting")
+#y=int(input())
+
+#if (y%400==0) or (y%4==0 and y%100!=0):
+#    print(366)
+#else:
+#    print(365)
+
+# if29
+#a = int(input("Son kiriting: "))
+
+#if a == 0:
+#    print("Son nolga teng")
+
+#elif a > 0:
+#    if a % 2 == 0:
+#        print("Musbat juft son")
+#    else:
+#        print("Musbat toq son")
+
+#else:
+ #   if a % 2 == 0:
+ #       print("Manfiy juft son")
+ #   else:
+ #       print("Manfiy toq son")
+
+# if30
+#n = int(input("1-999 oralig'ida son kiriting: "))
+
+#if n < 1 or n > 999:
+#    print("Xato son kiritildi")
+
+#else:
+    # xonalar soni
+ #   if n < 10:
+ #       xona = "bir xonali"
+  #  elif n < 100:
+   #     xona = "ikki xonali"
+  #  else:
+ #       xona = "uch xonali"
+
+    # juft yoki toq
+  #  if n % 2 == 0:
+   #     tur = "juft son"
+   # else:
+    #    tur = "toq son"
+
+   # print(xona, tur)
